513276c4cb61cef804fcf6d2e1ccca6accce2c97
DESKTOP-D02ETRR\ppsaltis <ppanas>
2022-05-02 15:19:00 +0200

    feat: develop - Total distance between ports

--- src/features/voyage-port.tsx
+++ src/features/voyage-port.tsx
@@ -1,59 +1,90 @@
-import { useAppSelector } from './../services/hooks'
+import { useAppDispatch,useAppSelector } from './../services/hooks'
+import { Port } from "./../services/ports-reducer"
+export const CALCULATE_DISTANCE_BETWEEN_PORTS = "REDUX_THUNK_CALCULATE_DISTANCE_BETWEEN_PORTS"
+
 
 export function VoyagePort(){
     const voyage = useAppSelector(state => state.voyage);
-
+    const dispatch = useAppDispatch();
+    
     return (
         <div className="voyage-listing">
             <h2>Voyage</h2>
 
             {
-            voyage.ports.length == 0 ? '' :
+            voyage.ports.length == 0 ? 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Source Port</th>
+                            <th>Destination Port</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>-</td>    
+                            <td>-</td>    
+                        </tr>
+                    </tbody>
+                </table>
+            :
                 voyage.ports.length == 1 ? 
                     <table>
                         <thead>
                             <tr>
                                 <th>Source Port</th>
                                 <th>Destination Port</th>
-                                <th>Distance</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>{voyage.ports[0].name}</td>    
-                                <td></td>    
-                                <td></td>    
+                                <td>-</td>    
                             </tr>
                         </tbody>
-                    </table>  
+                    </table>
                 :
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Source Port</th>
-                            <th>Destination Port</th>
-                            <th>Distance</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {voyage.ports.map(function(value, index, elements) {
-                            debugger;
-                            if (index +1 == elements.length) {
-                                return;
-                            }
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Source Port</th>
+                                <th>Destination Port</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {voyage.ports.map(function(value, index, elements) {
+                                if (index +1 == elements.length) {
+                                    return;
+                                }
 
-                            var next = elements[index+1];
-                            return (
-                                <tr>
-                                    <td>{ value.name }</td>
-                                    <td>{ next.name }</td>
-                                    <td>{ value.lat * next.lat }</td>
-                                </tr>
-                            );
+                                var next = elements[index+1];
+                                return (
+                                    <tr>
+                                        <td>{ value.name }</td>
+                                        <td>{ next.name }</td>
+                                    </tr>
+                                );
 
-                        })}
-                    </tbody>
-                </table>  
+                            })}
+                        </tbody>
+                    </table>  
+
+                    <button onClick={() => 
+                        dispatch( 
+                            {type: CALCULATE_DISTANCE_BETWEEN_PORTS, data: voyage.ports })
+                    }>Calculate Total Distance </button>
+
+                    {voyage.duration == 0 
+                        ? 
+                        '' 
+                        :
+                        <div>
+                            <h3>Total Distance</h3>
+                            <p>{voyage.duration} K.M</p>
+                        </div>
+                    }
+                </div>
             }
         </div>
     )
--- src/services/voyage-reducer.ts
+++ src/services/voyage-reducer.ts
@@ -1,6 +1,8 @@
 import { AnyAction } from "redux"
 import { Port } from "./ports-reducer"
-import { ADD_VOYAGE_PORTS } from "../features/voyage-planner"
+import { ADD_VOYAGE_PORTS,  } from "../features/voyage-planner"
+import { CALCULATE_DISTANCE_BETWEEN_PORTS  } from "../features/voyage-port"
+
 /**
  * This file controls the state of the "voyage" data.  A voyage consists of 0 or more
  * stops in a port.  You should calculate the total duration of the voyage as ports
@@ -18,13 +20,16 @@ interface VoyageState {
     duration: number,
 
     //ordered list of ports the vessel will stop at
-    ports: Port[]
+    ports: Port[],
+
+    totalDistance: number
 }
 
  
 const initialState: VoyageState = {
     duration: 0,
     ports: [],
+    totalDistance: 0
  }
 
  
@@ -36,7 +41,12 @@ const initialState: VoyageState = {
                 ports: addPort(state, action.data),
             };
         }
-        
+        case CALCULATE_DISTANCE_BETWEEN_PORTS: {
+            return {
+                ...state,
+                totalDistance: calculateDistanceBetweenSelectedPorts(state, action.data) 
+            };
+        }
         default: {
             return state;
         }
@@ -47,5 +57,46 @@ const initialState: VoyageState = {
     return state.ports.concat(port);
  }
 
+ export function calculateDistanceBetweenSelectedPorts(state: VoyageState, ports: Port[]): number {
+    debugger;
+    let totalDistance: number = 0
+
+    if (ports.length <= 1) // No ports or Only One 
+        return 0;
+
+    ports.map(function(value, index, elements) {
+        if (index +1 == elements.length) {
+            return;
+        }
+        let current = elements[index];
+        var next = elements[index+1];
+        
+        totalDistance += haversine(current.lat, current.lng, next.lat, next.lng)
+    })
+    
+    return totalDistance;
+ }
+
  export function removePort(port: Port){}
  export function movePort(port: Port, newPosition: number){}
+
+function haversine(lat1: number, lon1: number, lat2: number, lon2: number): number
+    {
+        // distance between latitudes
+        // and longitudes
+        let dLat = (lat2 - lat1) * Math.PI / 180.0;
+        let dLon = (lon2 - lon1) * Math.PI / 180.0;
+           
+        // convert to radiansa
+        lat1 = (lat1) * Math.PI / 180.0;
+        lat2 = (lat2) * Math.PI / 180.0;
+         
+        // apply formulae
+        let a = Math.pow(Math.sin(dLat / 2), 2) +
+                   Math.pow(Math.sin(dLon / 2), 2) *
+                   Math.cos(lat1) *
+                   Math.cos(lat2);
+        let rad = 6371;
+        let c = 2 * Math.asin(Math.sqrt(a));
+        return rad * c;
+    }
